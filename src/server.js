9a142ada8aba02561f49447f617841340838048f
Trey Hoover <treyhoover@gmail.com>
2018-01-25 14:03:25 -0500

    Copy gifs instead of using a volume...

--- src/server.js
+++ src/server.js
@@ -11,86 +11,89 @@ const app = express();
 const { REDIS_HOST = "127.0.0.1", REDIS_PORT = 6379 } = process.env;
 const PRICE_EXPIRATION = 15 * 60; // 15 minutes
 
-const client = redis.createClient({
-  host: REDIS_HOST,
-  port: REDIS_PORT,
-});
+(async () => {
 
-const getAsync = promisify(client.get).bind(client);
+  const client = redis.createClient({
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+  });
 
-const resolveTmp = (p) => path.resolve("/", "tmp", p);
+  const getAsync = promisify(client.get).bind(client);
 
-const daysLabelMap = {
-  "1": "24 hours",
-  "7": "week",
-  "30": "month",
-  "365": "year",
-};
+  const resolveTmp = (p) => path.resolve("/", "tmp", p);
 
-const fetchCoin = async ({ ts, symbol = "BTC" }) => {
-  const currentPriceKey = `${symbol}_PRICE_CURRENT`;
-  const isCurrentPriceReq = !ts;
+  const daysLabelMap = {
+    "1": "24 hours",
+    "7": "week",
+    "30": "month",
+    "365": "year",
+  };
 
-  // check redis first (historical requests only)
-  if (isCurrentPriceReq) {
-    const cachedPrice = await getAsync(currentPriceKey);
+  const fetchCoin = async ({ ts, symbol = "BTC" }) => {
+    const currentPriceKey = `${symbol}_PRICE_CURRENT`;
+    const isCurrentPriceReq = !ts;
 
-    if (cachedPrice) return Promise.resolve(cachedPrice);
-  }
-
-  return axios.get('https://min-api.cryptocompare.com/data/pricehistorical', {
-    params: {
-      ts,
-      fsym: symbol,
-      tsyms: "USD",
-    },
-  }).then(res => {
-    const price = res.data[symbol.toUpperCase()]["USD"];
-
+    // check redis first (historical requests only)
     if (isCurrentPriceReq) {
-      client.set(currentPriceKey, price, 'EX', PRICE_EXPIRATION);
+      const cachedPrice = await getAsync(currentPriceKey);
+
+      if (cachedPrice) return Promise.resolve(cachedPrice);
     }
 
-    return price;
-  });
-};
+    return axios.get('https://min-api.cryptocompare.com/data/pricehistorical', {
+      params: {
+        ts,
+        fsym: symbol,
+        tsyms: "USD",
+      },
+    }).then(res => {
+      const price = res.data[symbol.toUpperCase()]["USD"];
 
-app.get('/gif', async (req, res) => {
-  try {
-    const { symbol = "BTC", days = "1" } = req.query;
-    const date = new Date();
-    date.setDate(date.getDate() - days);
-    const ts = Date.parse(date.toDateString()) / 1000;
+      if (isCurrentPriceReq) {
+        client.set(currentPriceKey, price, 'EX', PRICE_EXPIRATION);
+      }
 
-    const fetchHistorical = fetchCoin({ symbol, ts });
-    const fetchCurrent = fetchCoin({ symbol });
-
-    const [prevPrice, currentPrice] = await Promise.all([fetchHistorical, fetchCurrent]);
-    const percentChange = (currentPrice / prevPrice - 1) * 100;
-    const q = percentToEmotion(percentChange);
-
-    const gifPath = await fetchGifForEmotion(q);
-
-    const upOrDown = percentChange >= 0 ? "up" : "down";
-    const color = upOrDown === "up" ? "green" : "red";
-    const percentRounded = Math.abs(Math.floor(percentChange)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    const pastTime = daysLabelMap[days];
-
-    await createGif({
-      text: `When ${symbol} is ${upOrDown} <span foreground="${color}" font_weight="bold">${percentRounded}%</span> in the past ${pastTime}...`,
-      src: gifPath,
-      outputFile: resolveTmp("output.gif"),
-      tmpTextOverlay: resolveTmp("overlay.png"),
-      fontSize: 72,
-      strokeWidth: 2,
+      return price;
     });
+  };
 
-    res.sendFile(resolveTmp("output.gif"))
-  } catch (e) {
-    console.error(e);
-  }
-});
+  app.get('/gif', async (req, res) => {
+    try {
+      const { symbol = "BTC", days = "1" } = req.query;
+      const date = new Date();
+      date.setDate(date.getDate() - days);
+      const ts = Date.parse(date.toDateString()) / 1000;
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
-});
+      const fetchHistorical = fetchCoin({ symbol, ts });
+      const fetchCurrent = fetchCoin({ symbol });
+
+      const [prevPrice, currentPrice] = await Promise.all([fetchHistorical, fetchCurrent]);
+      const percentChange = (currentPrice / prevPrice - 1) * 100;
+      const q = percentToEmotion(percentChange);
+
+      const gifPath = await fetchGifForEmotion(q);
+
+      const upOrDown = percentChange >= 0 ? "up" : "down";
+      const color = upOrDown === "up" ? "green" : "red";
+      const percentRounded = Math.abs(Math.floor(percentChange)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      const pastTime = daysLabelMap[days];
+
+      await createGif({
+        text: `When ${symbol} is ${upOrDown} <span foreground="${color}" font_weight="bold">${percentRounded}%</span> in the past ${pastTime}...`,
+        src: gifPath,
+        outputFile: resolveTmp("output.gif"),
+        tmpTextOverlay: resolveTmp("overlay.png"),
+        fontSize: 72,
+        strokeWidth: 2,
+      });
+
+      res.sendFile(resolveTmp("output.gif"))
+    } catch (e) {
+      console.error(e);
+    }
+  });
+
+  app.listen(3000, () => {
+    console.log('App listening on port 3000!')
+  });
+})();
