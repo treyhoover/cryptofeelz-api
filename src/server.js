2e4247fea1ab2365978627d69d940b8e5776a556
Trey Hoover <treyhoover@gmail.com>
2018-01-25 14:15:36 -0500

    Add retry strategy to redis

--- src/server.js
+++ src/server.js
@@ -16,6 +16,24 @@ const PRICE_EXPIRATION = 15 * 60; // 15 minutes
   const client = redis.createClient({
     host: REDIS_HOST,
     port: REDIS_PORT,
+    retry_strategy: (options) => {
+      if (options.error && options.error.code === 'ECONNREFUSED') {
+        // End reconnecting on a specific error and flush all commands with
+        // a individual error
+        return new Error('The server refused the connection');
+      }
+      if (options.total_retry_time > 1000 * 60 * 60) {
+        // End reconnecting after a specific timeout and flush all commands
+        // with a individual error
+        return new Error('Retry time exhausted');
+      }
+      if (options.attempt > 10) {
+        // End reconnecting with built in error
+        return undefined;
+      }
+      // reconnect after
+      return Math.min(options.attempt * 100, 3000);
+    }
   });
 
   const getAsync = promisify(client.get).bind(client);
